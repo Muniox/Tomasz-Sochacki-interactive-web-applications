207159d7202a06ee3f772de1bf404d2e09e32f11
Muniox <muniox20@gmail.com>
2024-02-06 16:16:41 +0100

    kolekcje referencji do elementów DOM

---
+++ roz3. Postawy pracy z elementami DOM/01.Pobieranie referencji do elementów DOM/script.js
@@ -0,0 +1,15 @@
+// pobieranie referencji za pomocą identyfikatora elementu (id)
+const login = document.getElementById('login');
+
+// pobieranie referncji za pomocą identyfikatora elementu przy pomocy funkcji querySelector
+const login2 = document.querySelector('#login');
+
+// pobieranie referencji za pomocą atrybutu klasy elementu
+const login3 = document.querySelector('.user-login-css-class');
+
+// pobieranie referencji za pomocą atrybutu data-
+const login4 = docuement.querySelector('[data-user-login]');
+const login5 = document.querySelector('[data-user-info="login"]');
+
+// pobieranie referencji za pomocą znacznika 'p'
+const login6 = document.querySelector('p');
